488c10bbd3ad88eeccd1d3b2792b5b1d7aabe61f
woosung1223 <dntjd991223@naver.com>
2022-12-05 15:10:59 +0900

    feat(Try): 시도 횟수 관련 검증기능 생성

--- src/main/java/racingcar/domain/Try.java
+++ src/main/java/racingcar/domain/Try.java
@@ -4,7 +4,10 @@ public class Try {
     private int count = 0;
     private int limit;
 
+    private final String TRY_IS_NOT_POSITIVE = "[ERROR] 시도 횟수는 한 번 이상이어야 합니다.";
+
     public Try(int limit) {
+        validate(limit);
         this.limit = limit;
     }
 
@@ -14,5 +17,15 @@ public class Try {
 
     public boolean cantTryAnymore() {
         return count >= limit;
+    }
+
+    private void validate(int limit) {
+        checkIsPositive(limit);
+    }
+
+    private void checkIsPositive(int limit) {
+        if (limit <= 0) {
+            throw new IllegalArgumentException(TRY_IS_NOT_POSITIVE);
+        }
     }
 }
