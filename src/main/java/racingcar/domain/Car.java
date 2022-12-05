1b412e7dd6ed188868d9b3cac3e3f8260c8f771e
woosung1223 <dntjd991223@naver.com>
2022-12-05 14:27:35 +0900

    feat(Car): 자동차를 이동시키는 기능 생성

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -4,6 +4,7 @@ public class Car {
     private final String name;
     private int position = 0;
 
+    private final int MOVE_POSSIBLE_VALUE = 4;
     private final int NAME_SIZE_LIMIT = 5;
     private final String NAME_SIZE_OUT_OF_RANGE = String.format("[ERROR] 자동차 이름은 %d글자 이하여야 합니다.", NAME_SIZE_LIMIT);
     private final String NAME_IS_EMPTY = "[ERROR] 자동차 이름은 빈 값이어서는 안됩니다.";
@@ -12,7 +13,13 @@ public class Car {
         validate(name);
         this.name = name;
     }
-    
+
+    public void move() {
+        if (RandomNumGenerator.makeRandomDigit() > MOVE_POSSIBLE_VALUE) {
+            position++;
+        }
+    }
+
     private void validate(String name) {
         checkSizeInRange(name);
         checkIsNotEmpty(name);
